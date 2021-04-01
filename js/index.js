eaf94399de62ada63a5365e23724054b06910173
Axel JARNIGON <axel.jarnigon@gmail.com>
2021-04-01 14:58:23 +0200

    slideshow/carousel working with pics, desc and btn

---
+++ js/index.js
@@ -0,0 +1,41 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function slideTotal(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var slides = document.getElementsByClassName("mySlides");
+    var indics = document.getElementsByClassName("indic");
+
+    // To return to 1st slide if pressing next on last slide
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    // To go to last slide if pressing previous on 1st slide
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    // Hiding all the slides
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    // Show on the indicators which one is active
+    for (i = 0; i < indics.length; i++) {
+        if (i === slideIndex-1) {
+            indics[i].style = "background-color: #505050";
+        }
+        else {
+            indics[i].style = "background-color: rgb(194, 194, 194)";
+        }
+    }
+    // Displaying the current slide
+    slides[slideIndex-1].style.display = "block";
+} 
